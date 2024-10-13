60c4cbcf5efce13f3e2ca83fd8fb7879056fda9a
Calvin Post <calvinator08@gmail.com>
2024-10-13 04:42:36 -0500

    Added migrate.rb

---
+++ migrate.rb
@@ -0,0 +1,20 @@
+require 'pg'
+
+db = PG.connect(ENV['DATEBASE_URL'])
+
+db.exec <<~SQL
+  CREATE TABLE lists (
+    id serial PRIMARY KEY,
+    name text UNIQUE NOT NULL
+  );
+
+  CREATE TABLE todos (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    completed boolean NOT NULL default false,
+    list_id integer REFERENCES lists (id)
+      ON DELETE CASCADE NOT NULL
+  );
+SQL
+
+db.disconnect
